85d248c574620d488931cb54c59f4e91e3f188b8
Xians <github@xians.su>
2024-01-04 23:38:56 +0800

    Update baseConstants.ts

--- src/constants/baseConstants.ts
+++ src/constants/baseConstants.ts
@@ -1,3 +1,3 @@
-export const baseURL = process.env.BASE_URL ?? "http://localhost:3000/";
+export const baseURL = process.env.BASE_URL ?? "https://vercel-og-beta.vercel.app/";
 export const baseImageURL =
   "https://raw.githubusercontent.com/XiansNotion/slowmur.com/main/public/2ab.png";
